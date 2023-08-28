83abb3499611ec9530672018decdd2c0ffb37c8c
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2023-08-28 16:32:01 +0530

    remove reset

--- app/src/main/java/com/shruti/colorlist/MainActivity.kt
+++ app/src/main/java/com/shruti/colorlist/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.SharedPreferences.Editor
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.shruti.colorlist.databinding.ActivityMainBinding
@@ -16,35 +17,42 @@ import com.github.dhaval2404.colorpicker.model.ColorShape
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var sharedPreference : SharedPreferences
-    lateinit var editor : SharedPreferences.Editor
+    lateinit var sharedPreference: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-       SingletonObject.sharedPrefs.initPreference(this)
-        sharedPreference = getSharedPreferences(resources.getString(R.string.app_name),Context.MODE_PRIVATE)
+        SingletonObject.sharedPrefs.initPreference(this)
+        sharedPreference =
+            getSharedPreferences(resources.getString(R.string.app_name), Context.MODE_PRIVATE)
         editor = sharedPreference.edit()
         binding.mainactivity = this
         binding.show = true
     }
-    fun buttonClick(){
-     print("Click Dialog")
-     var dialog = Dialog(this)
-     var dialogBinding = CustomDialogBinding.inflate(layoutInflater)
-     dialog.setContentView(dialogBinding.root)
-        dialogBinding.etcolorfirst.setOnClickListener{
-            ColorPickerDialog
-                .Builder(this)        				// Pass Activity Instance
-                .setTitle("Pick Theme")           	// Default "Choose Color"
-                .setColorShape(ColorShape.SQAURE)   // Default ColorShape.CIRCLE
-                .setDefaultColor(R.color.white)     // Pass Default Color
-                .setColorListener { color, colorHex ->
-                    // Handle Color Selection
-                    System.out.println("color $color colorHex $colorHex")
-                }
-        }
-     dialog.show()
- }
-    fun save(){}
+
+    fun buttonClick() {
+        print("Click Dialog")
+        var dialog = Dialog(this)
+        var dialogBinding = CustomDialogBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+        dialog.getWindow()?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    fun save() {}
+    fun saveColorFirst() {
+        ColorPickerDialog
+            .Builder(this)                        // Pass Activity Instance
+            .setTitle("Pick Theme")            // Default "Choose Color"
+            .setColorShape(ColorShape.SQAURE)   // Default ColorShape.CIRCLE
+            .setDefaultColor(R.color.white)     // Pass Default Color
+            .setColorListener { color, colorHex ->
+                // Handle Color Selection
+                System.out.println("color $color colorHex $colorHex")
+            }
+    }
+
 }
